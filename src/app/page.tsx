9c74eea16edc2dacd402f98406d23969fa5c93a6
Marc Veens <marcveens@hotmail.com>
2024-05-15 23:06:52 +0200

    Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,28 @@ export default function Home() {
 
   return (
     <main className="mx-auto flex max-w-[600px] flex-col items-center justify-between px-4 py-12">
-      <h1 className="mb-10 text-4xl font-bold">Random Wiki Articles</h1>
+      <div className="mb-10">
+        <h1 className="text-4xl font-bold">Random Wiki Articles</h1>
+        <div className="flex items-center justify-center gap-2">
+          <a
+            href="https://github.com/marcveens/random-wikipedia-article-of-the-day"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm text-gray-400 underline hover:text-violet-300"
+          >
+            Source
+          </a>
+          <span>&#8226;</span>
+          <a
+            href="https://www.marcveens.nl/posts/scheduled-data-scraping-made-easy-using-playwright-with-github-actions"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm text-gray-400 underline hover:text-violet-300"
+          >
+            Blog post
+          </a>
+        </div>
+      </div>
 
       <div className="flex w-full flex-col gap-8">
         {list.map((record) => {
@@ -22,16 +43,16 @@ export default function Home() {
 
           return (
             <div key={record.date}>
-              <p className="text-sm text-gray-500">{formattedDate}</p>
+              <p className="text-sm text-gray-400">{formattedDate}</p>
               <a
                 href={record.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex flex-col gap-2 transition-colors duration-200 hover:text-violet-600"
+                className="flex flex-col gap-2 transition-colors duration-200 hover:text-violet-300"
               >
                 <h2 className="text-2xl font-semibold">{record.title}</h2>
               </a>
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap">{record.summary}</p>
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-gray-200">{record.summary}</p>
             </div>
           );
         })}
